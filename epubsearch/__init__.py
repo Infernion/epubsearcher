d38c3c2c83ff37c16567b62bd7d78bf821402933
Serhii Khalymon <sergiykhalimon@gmail.com>
2015-03-27 17:05:57 +0200

    refactored

--- epubsearch/__init__.py
+++ epubsearch/__init__.py
@@ -1,5 +1,4 @@
 from .epubparser import EpubParser
 from .epubindexer import EpubIndexer
-from .epubretriever import EpubRetriever
 from .wordmorpho import WordMorphoGenerator
 from .crossdomain import crossdomain
