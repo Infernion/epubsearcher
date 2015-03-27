18cbbf2aab1c2d374bb53343261e8890385a793a
Serhii Khalymon <sergiykhalimon@gmail.com>
2015-03-27 17:06:35 +0200

    added unittests

--- test.py
+++ test.py
@@ -1,17 +1,32 @@
 # -*- coding: utf-8 -*- 
-from epubsearch import EpubParser
-from epubsearch import EpubIndexer
-# from epubsearch import EpubRetriever
+from main import EpubWorker
+import unittest
+# import subprocess
+
+class TestMainPy(unittest.TestCase):
+    def setUp(self):
+        pass
 
 
-epub = EpubParser("Sensei4")
+WRONG_REQUEST = {'results': []}
+RIGHT_REQUEST = {'baseCfi': '/6/24[id15]!', 'title': '', 'path': './tmp/Sensei4//index_split_011.html',
+                              'href': 'index_split_011.html', 'cfi': '/6/24[id15]!/4/62'}
 
-index = EpubIndexer("whoosh")
-index.load(epub)
+class TestEpubWorker(unittest.TestCase):
+    def setUp(self):
+        test_dir = './test_data/'
+        self.epub_dir = EpubWorker(test_dir + 'Sensei4/')
+        self.epub_file = EpubWorker(test_dir + 'Sensei4.epub')
 
-results = index.search(u"аллат", 2)
-if results:	
-	with open('logs', 'w+') as f:
-		f.write(str(results).decode("utf-8"))
-    	print results
+    def test_search_in_dir(self):
+        self.assertDictEqual(self.epub_dir.search_word('хряк'), WRONG_REQUEST)
+        self.assertDictEqual(self.epub_dir.search_word('аллат')['results'][0], RIGHT_REQUEST)
+
+    def test_search_in_epub(self):
+        self.assertDictEqual(self.epub_file.search_word('хряк'), WRONG_REQUEST)
+        self.assertDictEqual(self.epub_file.search_word('аллат')['results'][0], RIGHT_REQUEST)
+
+
+if __name__ == '__main__':
+    unittest.main()
 
